a4667612cb7535278cf090eea4decf000922bdb6
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-12-11 18:10:37 -0500

    Form with css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import styles from './App.module.css';
+import Form from './components/Form/Form';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+    
+      <div className={styles.container}>
+        <Form />  
+
+      </div>
+    </>
   )
 }
 
