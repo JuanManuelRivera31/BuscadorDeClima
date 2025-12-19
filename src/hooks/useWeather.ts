5646cd4aff6a16bee66690ed64b65db1fa6d498a
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-12-19 14:43:12 -0500

    Estudiamos y aplicamos distintas maneras de tipar los resultados de nuestra API, desde la peor hasta mejor, sin librerias (Type Guards) y con librerias (Zod, Valibot) para finalmente implementar Zod en nuestra aplicación por simplicar el proceso

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,50 @@
 import axios from 'axios';
 import type { SearchType } from '../types';
+import { z } from 'zod'
+// import { object, string, number, Output, parse } from 'valibot'
+
+// TYPE GUARD O ASSERTION
+// function isWeatherResponse(weather : unknown) : weather is Weather {
+//     return (
+//         Boolean(weather) &&
+//         typeof weather === 'object' && 
+//         typeof (weather as Weather).name === 'string' &&
+//         typeof (weather as Weather).main.temp === 'number' &&
+//         typeof (weather as Weather).main.temp_max === 'number' &&
+//         typeof (weather as Weather).main.temp_min === 'number' 
+//     )
+// } 
+
+// Zod
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })
+})
+export type Weather = z.infer<typeof Weather>
+
+// Valibot
+// const WeatherSchema = object({
+//     name: string(),
+//     main: object({
+//         temp: number(),
+//         temp_max: number(),
+//         temp_min: number()
+//     })
+// })
+// type Weather = Output<typeof WeatherSchema>
+
+const initialState = {
+    name: '',
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0
+    }
+}
 
 export default function useWeather() {
     
@@ -17,9 +62,33 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
             
+            // Castear el type
+            // const {data: weatherResult} = await axios<Weather>(weatherUrl)
+            // console.log(weatherResult.temp)
+            // console.log(weatherResult.name)
+
+            // Type Guards
+            // const {data: weatherResult} = await axios(weatherUrl)
+            // const result = isWeatherResponse(weatherResult)
+            // if(result) {
+            //     console.log(weatherResult.name)
+            // } else {
+            //     console.log('Respuesta mal formada')
+            // }
+
+            // Zod
             const {data: weatherResult} = await axios(weatherUrl)
-            
-            console.log(weatherResult);
+            const result = Weather.safeParse(weatherResult)
+            if(result.success) {
+                // setWeather(result.data)
+            }
+
+            // Valibot
+            // const {data: weatherResult} = await axios(weatherUrl)
+            // const result = parse(WeatherSchema, weatherResult)
+            // if(result) {
+            //     console.log(result.name)
+            // }
             
         } catch (error) {
 
