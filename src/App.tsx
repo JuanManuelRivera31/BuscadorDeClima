59ddba16c617814c5b84c5f089d0430a0034a5e1
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-12-19 00:55:30 -0500

    Form validado y con su state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import styles from './App.module.css';
-import Form from './components/Form/Form';
+import Form from './components/Form/Form.tsx';
 
 function App() {
+  const fetchWeather = async () => {
+    // tu lógica para llamar el API y setear estado
+  };
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
     
       <div className={styles.container}>
-        <Form />  
+        <Form fetchWeather={fetchWeather} />
 
       </div>
     </>
