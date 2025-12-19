29ecaec1c38084d0bd2a9f52bedc207c60c714ea
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-12-19 02:18:38 -0500

    Llamado doble existoso nuestra API, procedemos a typar sus resultados

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import styles from './App.module.css';
 import Form from './components/Form/Form.tsx';
+import useWeather from './hooks/useWeather.ts';
 
 function App() {
-  const fetchWeather = async () => {
-    // tu lógica para llamar el API y setear estado
-  };
+  
+  const { fetchWeather } = useWeather()
+
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
     
       <div className={styles.container}>
-        <Form fetchWeather={fetchWeather} />
+        <Form 
+          fetchWeather={fetchWeather} 
+        />
 
       </div>
     </>
